3090e1be0a07249704c53c1a0f6153dac19501cd
arkanayudhistira <arkana.yudhistira@gmail.com>
2023-08-02 15:50:53 +0700

    Created Repository

---
+++ assets.py
@@ -0,0 +1,234 @@
+class Classwork:
+    def __init__(self):
+        self.administration_deck = {
+'Administration Deck':
+'''Dear Participants,
+
+Berikut beberapa hal yang perlu Bapak Ibu ketahui mengenai administrasi selama mengikuti kelas Algoritma Academy.
+
+Best Regards,
+Team Algoritma''',
+
+'How to Ask Questions':
+'''Dear Participants,
+
+Algoritma Academy menyediakan tempat / forum untuk Bapak dan Ibu bertanya jika mengalami kesulitan / kendala / error / permasalahan.  Pengajuan pertanyaan teknikal (code/error) dapat melalui GitHub Algoritma Community (https://github.com/teamalgoritma/community). Panduan untuk bertanya terdapat pada laman tersebut. Sementara itu, pertanyaan terkait administrasi dapat diajukan melalui email ke mentor@algorit.ma
+
+Demikian informasi yang dapat kami sampaikan. Jika Bapak dan Ibu memiliki pertanyaan lebih lanjut, silahkan berikan komentar pada postingan ini. Terima kasih.
+
+Best Regards,
+Team Algoritma''',
+
+'Permohonan Sesi Mentoring':
+'''
+Dear Participants,
+
+Algoritma Academy memiliki fasilitas group mentoring, group mentoring merupakan  suplementary study bersama mentor dari Team Algoritma pada waktu dan jam yang telah ditentukan pada setiap minggunya. Sesi ini bertujuan membantu anda untuk memahami materi yang belum anda pahami pada saat kelas.
+
+Sesi group mentoring secara umum diadakan setiap hari Jumat pukul 13:00 - 15:00 WIB (sesi siang) dan pukul 18:30 - 20:30 WIB (sesi malam). Student kelas malam (night class) dapat mengikuti sesi mentoring yang ada di siang hari, begitu juga sebaliknya. Student hanya diperbolehkan menghadiri 1 sesi grup mentoring tiap minggunya. Contoh, student A sudah menghadiri sesi siang pada mentoring minggu tersebut, maka student A tidak diperkenankan mengikuti mentoring sesi malam. Bila hari Jumat libur, maka sesi group mentoring akan diundur ke hari Senin di jam yang sama, sehingga pada minggu tersebut ada kemungkinan dua kali group mentoring. Terlampir jadwal mentoring group beserta saran sesi mentoring yang dapat dihadiri oleh student. 
+
+Secara umum group mentoring dilaksanakan via Zoom dan link akan dibagikan melalui stream Classroom pada hari mentoring. Tersedia pula group mentoring on site khusus pada sesi malam pukul 18:30-20:30 WIB. Student yang ingin menghadiri mentoring secara on site perlu mengisi form pengajuan mentoring di bit.ly/mentoring-onsite. Permintaan mentoring on site dilakukan pada hari Senin-Jumat pukul 09:00-17:00 WIB, dan paling lambat H-1 sebelum hari mentoring yang diinginkan.
+
+Student yang menghadiri sesi mentoring disarankan telah mengajukan pertanyaan yang ingin dibahas melalui GitHub Algoritma Community (https://github.com/teamalgoritma/community). Panduan untuk bertanya, terdapat pada laman tersebut.  
+
+Apabila peserta memiliki pertanyaan lebih lanjut, silahkan berikan komentar pada postingan ini.
+Demikian yang dapat kami sampaikan.
+
+Best Regards,
+Team Algoritma
+'''
+
+}
+        
+        self.quiz = {
+'3. Q: Interactive Plotting (IP)':
+'''Dear class,
+
+Congratulations! You have completed the Interactive Plotting workshop, well done! However, you have to complete the hands-on quiz as your final project for this unit.
+
+As you have completed the quiz, please mark this assignment as done.
+
+Don't stop learning by building and good luck!!''',
+
+'2. Q: Data Visualization (DV)':
+'''Dear class,
+
+Congratulations! You have completed the Data Visualization workshop, well done! However, you have to complete the hands-on quiz as your final project for this unit.
+
+As you have completed the quiz, please mark this assignment as done.
+
+Don't stop learning by building and good luck!''',
+
+'1. Q: Programming for Data Science (P4DS) & Practical Statistic (PS)':
+'''Dear class,
+
+Congratulations! You have completed the Programming for Data Science (P4DS) & Practical Statistic (PS) workshop, well done! However, you have to complete the hands-on quiz as your final project for this unit.
+
+As you have completed the quiz, please mark this assignment as done.
+
+Don't stop learning by building and good luck!!''',
+
+'0. Q: Prerequisite Check':
+'''Dear Bapak/Ibu,
+
+Setelah berhasil melakukan installasi R dan RStudio, serta menginstall package R Markdown (instruksi terdapat pada Video Pre Class R), kami mohon kesediaannya untuk menuntaskan Prerequisite Check, berupa checkpoint yang memastikan bahwa Bapak/Ibu tidak mengalami kendala dalam mempersiapkan hal-hal sebelum kelas dimulai.
+
+Silahkan unggah screenshoot tampilan RStudio beserta halaman R Mardown baru pada assignment ini. Terlampir contoh screenshoot tampilan RStudio dengan file R Markdown baru.
+
+Terima kasih atas kerjasama Bapak/Ibu dan sampai jumpa di kelas!
+
+Best regards,
+Team Algoritma'''
+},
+
+        self.lbb = {
+'1. LBB: Programming for Data Science':
+'''Dear Bapak/Ibu,
+
+Selamat atas keberhasilannya melewati 3 hari pembelajaran di kelas Programming for Data Science! Dalam rangka meningkatkan kemampuan Bapak dan Ibu dalam mengimplementasikan ilmu yang didapatkan di kelas, kami menyediakan modul Learn by Building sebagai sarana Bapak dan Ibu untuk berlatih dalam membuat suatu reporting yang baik menggunakan R Markdown beserta analisis data sederhana.
+
+Berikut merupakan beberapa contoh reporting serta analisis sederhana menggunakan R yang dapat Bapak Ibu gunakan sebagai bahan acuan:
+
+- http://rpubs.com/wkania/EDA_Retail: Melakukan exploratory data analysis menggunakan data retail
+- http://rpubs.com/inayatus/p4ds-reyna: Analisis mengenai Instant Noodle di Indonesia
+- https://rpubs.com/l_tresya/1002515 : Analisis mengenai penjualan properti di Spanyol
+
+Kami sangat menyarankan bagi Bapak/Ibu yang berencana untuk ikut serta dalam Algoritma Career Support agar dapat mengerjakan modul tambahan ini sehingga dapat ditambahkan sebagai portofolio pribadi. Hal tersebut akan sangat membantu recruiter untuk memahami tingkat pengalaman case hands-on Bapak/Ibu.
+
+Bapak Ibu dapat menggunakan dataset `retail.csv` yang ada pada materi Programming for Data Science atau dapat menggunakan data yang Bapak Ibu miliki. Berikut beberapa referensi dataset yang bisa Bapak/Ibu gunakan pada modul ini:
+
+- https://archive.ics.uci.edu/ml/datasets/bank+marketing/ Data tersebut merupakan data mengenai telemarketing dari sebuah bank di Portugal. Menggunakan data tersebut kita dapat memprediksi calon nasabah mana yang akan membeli product ketika di telepon oleh pihak bank.
+- https://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones/ Data tersebut berisi tentang data pergerakan manusia yang di rekam menggunakan akselerometer dan gyroscope yang terdapat pada smartphone. Dengan menggunakan Informasi tersebut Bapak/Ibu dapat menklasikifikasikan manusia tersebut sedang melakukan aktivitas apa.
+- https://www.kaggle.com/c/titanic/ Dataset tersebut berisi tentang biodata dari penumpang kapal titanic, dari biodata tersebut apakah dapat digunakan untuk memprediksi apakah penumpang tersebut selamat atau tidak.
+- atau menggunakan data pilihan Bapak atau Ibu sendiri.
+
+Dalam pembuatan report, jangan lupa untuk meliputi hal-hal berikut:
+- memberikan markdown title, author name, dan date
+- melakukan formating style yang Bapak Ibu sukai
+- memberikan heading pada setiap bab dan sub bab yang dibuat dalam masing-masing pembahasannya
+- melakukan import data yang Bapak Ibu gunakan
+- melakukan penjelasan terhadap data yang digunakan
+- melakukan cleansing data dan exploratory terhadap data berdasarkan business problem yang Bapak Ibu buat
+- menggunakan statistik deskriptif atau inferensial untuk analisis data
+
+Berikut adalah referensi untuk mengatur format report pada R Markdown:
+- YAML R Markdown: https://bookdown.org/yihui/rmarkdown/html-document.html
+- Themes R Markdown: https://www.datadreaming.org/post/r-markdown-theme-gallery/
+- R Markdown Cheatsheet: https://raw.githubusercontent.com/rstudio/cheatsheets/master/rmarkdown-2.0.pdf
+- Tips R Markdown Reporting: https://holtzy.github.io/Pimp-my-rmd/
+
+Hasil report analysis yang telah dikerjakan, silahkan diunggah ke dalam salah satu situs publikasi pilihan Bapak/Ibu (Github / Rpubs / Kaggle Kernel), kemudian silahkan mencantumkan linknya saat melakukan Submit LBB. Link kepada situs publikasi pilihan Bapak/Ibu akan kami cantumkan pada CV Algoritma yang akan kami buatkan sebagai salah satu bagian dari Algoritma Career Support. Kami akan memberikan feedback terkait pekerjaan Bapak/Ibu yang mengumpulkan modul Learn by Building ini mengenai hal-hal yang dirasa bisa dikembangkan.
+
+Jika ada pertanyaan yang ingin Bapak/Ibu tanyakan, silahkan dapat email kami di mentor@algorit.ma. Kami akan dengan senang hati membantu.
+
+Terima kasih dan good luck!''',
+
+'2. LBB: Data Visualization':
+'''Dear Bapak/Ibu,
+
+Selamat atas keberhasilannya melewati 4 hari pembelajaran di kelas Data Visualization! Dalam rangka meningkatkan kemampuan Bapak dan Ibu dalam mengimplementasikan ilmu yang didapatkan di kelas, kami menyediakan modul Learn by Building sebagai sarana Bapak dan Ibu untuk berlatih membuat sebuah visualisasi yang estetik dan insightful.
+
+Berikut merupakan beberapa contoh visualisasi yang dapat Bapak Ibu gunakan sebagai bahan referensi:
+
+- http://rpubs.com/wkania/DV_NYC_Airbnb: Data Visualization - Airbnb NYC.
+- http://rpubs.com/WulanAndriyani/DV-PrimaditaningtyasW: Google Play Store Data Visualization.
+
+Kami sangat menyarankan bagi Bapak/Ibu yang berencana mengikuti Algoritma Career Support agar dapat mengerjakan modul tambahan ini sehingga dapat ditambahkan sebagai portofolio pribadi. Hal tersebut akan sangat membantu recruiter untuk memahami tingkat pengalaman case hands-on Bapak/Ibu.
+
+Bapak Ibu dapat menggunakan dataset `USvideos.csv` yang ada pada materi Data Visualization atau dapat menggunakan data yang Bapak Ibu miliki. Berikut beberapa referensi dataset yang bisa Bapak/Ibu gunakan pada modul ini:
+
+- https://archive.ics.uci.edu/ml/datasets/bank+marketing Data tersebut merupakan data mengenai telemarketing dari sebuah bank di Portugal. Menggunakan data tersebut kita dapat memprediksi calon nasabah mana yang akan membeli product ketika di telepon oleh pihak bank.
+- https://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones Data tersebut berisi tentang data pergerakan manusia yang di rekam menggunakan akselerometer dan gyroscope yang terdapat pada smartphone. Dengan menggunakan Informasi tersebut Bapak/Ibu dapat menklasikifikasikan manusia tersebut sedang melakukan aktivitas apa.
+- https://www.kaggle.com/c/titanic . Dataset tersebut berisi tentang biodata dari penumpang kapal titanic, dari biodata tersebut apakah dapat digunakan untuk memprediksi apakah penumpang tersebut selamat atau tidak.
+- atau menggunakan data pilihan Bapak atau Ibu sendiri.
+
+Dalam pembuatan report, jangan lupa untuk meliputi hal-hal berikut:
+
+- Tahapan data pre-processing yang dilakukan sebelum membuat visualisasi
+- Insight yang ingin disampaikan
+- Kesesuaian jenis plot yang digunakan
+- Estetika visualisasi yang ditampilkan:
+    - Penggunaan warna (fill/color) dan penyesuaian tema (theme)
+    - Pemberian label yang sesuai dengan informasi:
+- Label dari sumbu x / y
+- Title/Subtitle
+- Legend, etc.
+
+Hasil report analysis yang telah dikerjakan, silahkan diunggah ke dalam salah satu situs publikasi pilihan Bapak/Ibu (Github / Rpubs / Kaggle Kernel), kemudian silahkan mencantumkan linknya saat melakukan Submit LBB. Link situs publikasi pilihan Bapak/Ibu akan kami cantumkan pada CV Algoritma yang akan kami buatkan sebagai salah satu bagian dari Algoritma Career Support. Kami akan memberikan feedback untuk bagian-bagian yang dirasa dapat dikembangkan terkait Learn by Building yang Bapak/ibu kumpulkan.
+
+Jika ada pertanyaan yang ingin Bapak/Ibu tanyakan, silahkan mengirim email ke mentor@algorit.ma. Kami akan dengan senang hati membantu.
+
+Terima kasih an good luck!''',
+
+'3. LBB: Interactive Plotting':
+'''Dear Bapak/Ibu,
+
+Selamat atas keberhasilannya melewati 4 hari pembelajaran di kelas Interactive Plotting! Dalam rangka meningkatkan kemampuan Bapak dan Ibu dalam mengimplementasikan ilmu yang didapatkan di kelas, kami menyediakan modul Learn by Building sebagai sarana Bapak dan Ibu untuk berlatih membuat sebuah interactive dashboard (flexdashboard/shiny/shinydashboard) yang estetik dan insightful.
+
+Berikut merupakan beberapa contoh dashboard yang dapat Bapak Ibu gunakan sebagai bahan referensi:
+
+- https://nplaybook.shinyapps.io/earth_quake/
+- https://risalandika.shinyapps.io/LBB_IP_HYDRA_NIGHT_B_RISAL_ANDIKA/
+- https://gulistianifuzuli.shinyapps.io/LBB_IP_Gulis/
+- https://nicolasgomasjaya.shinyapps.io/videogames/
+
+Bapak Ibu dapat menggunakan dataset `youtubetrends.csv` yang ada pada materi interactive plotting atau dapat menggunakan data yang Bapak Ibu miliki. Berikut beberapa referensi mengenai dashboard untuk mempermudah pembuatan LBB:
+
+- https://shiny.rstudio.com/gallery/
+- https://rstudio.github.io/shinydashboard/structure.html
+- https://www.tychobra.com/shiny/
+- https://fontawesome.com/icons?from=io
+
+Berikut point penilaian untuk LBB Interactive Plotting:
+
+- Tahapan data pre-processing menggunakan dplyr
+- Plot yang ditampilkan pada dashboard sudah interaktif
+- Setiap plot yang ditampilkan menampilkan informasi yang relevan dari dashboard
+
+Hasil dashboard yang telah dikerjakan, jika menggunakan flexdashboard silahkan unggah di rpubs, dan jika menggunakan shinydashboard dapat dilampirkan folder yang digunakan (dataset, global.R, ui.R, server.R) atau menyertakan link shiny. Link situs publikasi pilihan Bapak/Ibu akan kami cantumkan pada CV Algoritma yang akan kami buatkan sebagai salah satu bagian dari Algoritma Career Support. Kami akan memberikan feedback untuk bagian-bagian yang dirasa dapat dikembangkan terkait Learn by Building yang Bapak/Ibu kumpulkan.
+
+Jika ada pertanyaan yang ingin Bapak/Ibu tanyakan, silahkan mengirim email ke mentor@algorit.ma. Kami akan dengan senang hati membantu.
+
+Terima kasih dan good luck!'''
+},
+        self.lbb = {
+'Brief Capstone Data Visualization':
+'''Dear Bapak/Ibu,
+
+Brief Capstone Data Visualization (DV) merupakan file pemaparan rancangan Capstone DV yang hendak Bapak/Ibu buat. Brief ini di buat pada hari pemaparan Capstone DV. Petunjuk pengerjaan Capstone DV dapat diakses di link berikut: https://rpubs.com/AlgoritmaAcademy/dv-capstone
+
+Jika sudah menyelesaikkan Brief Capstone Data Visualization, silahkan knit ke dalam versi .html dan submit file .html tersebut di sini. Bapak/Ibu diiharapkan mengikuti template yang sudah dilampirkan di bawah ini.
+
+Demikian yang dapat kami sampaikan. Terimakasih atas perhatian Bapak/Ibu.
+
+Best regards,
+Team Algoritma''',
+
+'Capstone Data Visualization':
+'''Dear Bapak/Ibu,
+
+Dear Participants,
+
+Today is the day of the Capstone Data Visualization. Congratulations! You almost finished the Data Visualization track. You can find the full description for Data Visualization Capstone Project in the link attached. Here are the rubrics for your final project :
+
+1. Input (reactivity) : 6 points
+2. Tab (paging) : 3 points
+3. Render plot (plotly/leaflet) : 9 points
+4. Deploy : 6 points
+5. UI Appearance : 6 points
+Total : 30 points
+
+Capstone contain majority of your mark in the DV track score (30 out of 40). Therefore, you must complete the Capstone project as best as you can. For more details about the rubrics you can follow the link below.
+
+After you finished your assignment, you can submit your zip files that contains all scripts, data, .Rmd, and html to google classroom and comment your shiny app link. Any submission later than the deadline will gain penalty as follows:
+
+- 1 Day late: minus 1 point
+- 2 Days late: minus 3 points
+- 3 Days late: minus 5 points
+- 4 Days late: minus 7 points
+- >4 Days late: minus 9 points
+
+Students whose score is below 24 will be given 1 chance to resubmit their work. Resubmission can be done no later than 1 week after the initial score is revealed. Students who request resubmission can only score a maximum of 24 points (penalty points from late submission will also be calculated for the final score). Submission later than 1 month after the deadline will not be scored.
+
+You can comment in this post or email mentor@algorit.ma if you have any questions for the details and We’ll be happy to help! Good luck!''',
+}
